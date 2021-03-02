cb6de952289e68abd585a2fff7c919acd2ea5117
Superxpdude <superxpdude@gmail.com>
2021-03-02 15:34:23 -0600

    Added basic repository files

---
+++ main/config.cpp
@@ -0,0 +1,16 @@
+#include "script_mod.hpp"
+
+class CfgPatches {
+	class sxp_utils_main {
+		name = "SXP Server Utils - Main";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = REQUIRED_VERSION;
+		requiredAddons[] = {};
+		author = "Superxpdude";
+		url = "https://github.com/Superxpdude/sxp-a3-server-utils";
+		version = VERSION;
+		versionStr = QUOTE(VERSION_STR);
+		versionAr[] = {VERSION_AR};
+	};
+};
---
+++ main/script_mod.hpp
@@ -0,0 +1,10 @@
+#include "script_version.hpp"
+
+#define VERSION      MAJOR.MINOR
+#define VERSION_STR  MAJOR.MINOR.PATCHLVL.BUILD
+#define VERSION_AR   MAJOR,MINOR,PATCHLVL,BUILD
+
+#define QUOTE(var1) #var1
+
+// Required Arma 3 version
+#define REQUIRED_VERSION 2.02
